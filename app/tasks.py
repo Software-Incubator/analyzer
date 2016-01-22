c829230c0e54a3b152580563bf8dc7b613259971
Raghvendra Tripathi <tripathiraghvendra604@gmail.com>
2016-01-23 01:31:29 +0530

    * modifications in marks dictionary  now each sem marks is a dict which contains a keys as subject codes and values as a list containing external, internal and credit mark

--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,4 @@
-import requests, urllib2 ,urllib, os,re
-import random
+import requests, urllib2 ,urllib, os
 from bs4 import BeautifulSoup
 #from .models import *
 
@@ -43,70 +42,82 @@ def get_results():
         print "Branch Name: ", branch_name
         print "Branch Code: ", branch_code
 
-        student_json = {
-            'st_id': roll_no,
-            'name': name,
-            'father_name': fathers_name,
-            'branch': branch_name,
-            'college': colg_code,
-            'sem': 3,
-            'marks': dict(),
-           # 'max_marks': max_marks
-
-        }
-
         #models.connection.User.insert(student_json)
         # for marks
         marks_tables = soup.find_all('table')[1].find_all('td', width='50%')
         flag = True
+        # marks is a list whose first element is odd sem marks,
+        # second element is even sem marks and third element is even sem marks and third is sum of all marks
+        marks = []
+        mark_odd = {}  # dictionary for odd sem marks
+        mark_even = {} # dictionary for even sem marks
+        # this loop iterates on both the tables
+        flag = True
         for marks_table in marks_tables:
-            print("Odd semester marks" if flag else "Even semester marks")
-            flag = False
             in_tables = marks_table.find_all('table')
             sem = (in_tables[0].find_all('tr')[0].find('th').string).split()[0][0]
             print 'semester is ' + sem
             in_table = in_tables[1]
             marks_rows = in_table.find_all('tr')[1:]
+            # this loop iterates on all rows of a particular table
             for marks_row in marks_rows:
                 row_cells = marks_row.find_all('span')
-                sub_code = row_cells[0].string
+                sub_code = row_cells[0].string.split()[0]
                 sub_name = row_cells[1].string
+                mark_list = []
+
                 try:
                     external = int(row_cells[2].string)
                 except ValueError:
                     external = 0
+                mark_list.append(external)
                 try:
                     internal = int(row_cells[3].string)
                 except (ValueError, IndexError):
                     internal = 0
+                mark_list.append(internal)
                 try:
                     carry_over = int(row_cells[4].string)
                 except (ValueError, TypeError, IndexError):
                     carry_over = 0
                 try:
                     credit = int(row_cells[5].string)
-                except (ValueError, IndexError,TypeError):
+                except (ValueError,TypeError):
                     credit = 0
+                except IndexError:
+                    credit = 0
+                mark_list.append(credit)
 
-                '''print sub_code
-                print sub_name
-                print external
-                print internal
-                print carry_over
-                print credit
-                print '****************'  '''
+                if flag:
+                    mark_odd[sub_code] = mark_list
+                else:
+                    mark_even[sub_code] = mark_list
+            flag = False
+
+        max_marks = int(soup.find(id ='ctl00_ContentPlaceHolder1_lblSTAT_8MRK').string)
+        marks = [mark_odd, mark_even,max_marks ]
+        print marks
 
         carry_papers = soup.find(
                 id='ctl00_ContentPlaceHolder1_lblCarryOver').text.split(',')
 
         carry_papers = [carry_code.rstrip() for carry_code in carry_papers]
-        print carry_papers
-        max_marks = int(soup.find(id ='ctl00_ContentPlaceHolder1_lblSTAT_8MRK').string)
-        print max_marks
+        # print carry_papers
+        # print max_marks
+
+        student_data = {
+            'roll_no': roll_no,
+            'name': name,
+            'father_name': fathers_name,
+            'branch': branch_name,
+            'college': colg_code,
+            'year': 3,
+            'marks': dict(),
+           # 'max_marks': max_marks
+            }
 
 
 def dnld_captcha(imageurl):
-   #x = random.randrange(1,1000)
    name = 'raghav.gif'
    urllib.urlretrieve(imageurl, os.path.join(os.getcwd(), name)) # download and save image
 
