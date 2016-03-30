4d88193feba6b15ad0690ac0d10061c2fa73e1ee
tripathiraghvendra604 <tripathiraghvendra604@gmail.com>
2016-03-31 00:24:19 +0530

    add college codes, branch codes names

--- config.py
+++ config.py
@@ -1,7 +1,24 @@
 # branch codes of the branches that exist in college
 BRANCH_CODES = ['00', '10', '13', '21', '31', '32', '40']
 
+COLLEGE_CODENAMES = {
+    '027': 'AKGEC',
+    '029': 'KIET',
+    '032': 'ABES',
+    '091': 'JSS'
+}
+
 BRANCH_NAMES = {"00": "Civil Engineering", }
+
+BRANCH_CODENAMES = {
+    "00": "CE",
+    "10": "CSE",
+    "13": "IT",
+    "21": "EN",
+    "31": "ECE",
+    "32": "EI",
+    "40": "ME"
+}
 
 URLS = {
     1: ("http://aktu.ac.in/results/gbturesult_11_12/Odd"
